77749a9c70942b3c7167639bba9694acf3d5419a
chandanaraj pattabiraman <chandanaraj@bicsglobal.com>
2020-03-25 12:00:13 +0530

    second commit

--- App/components/App.jsx
+++ App/components/App.jsx
@@ -1,18 +1,3 @@
-// import React from 'react';
-
-// import ApiContainer from './App/Screens/ApiContainer';
-
-// const App = () => {
-//   return (
-//     <>
-//       <ApiContainer />
-//     </>
-//   );
-// };
-
-
-// export default App;
-
 import React from 'react';
 import Button from '../App/containers/Button';
 import NewsItem from '../App/containers/NewsItem'
